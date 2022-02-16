1a4f31f70abf799acee0928d18721fa9c1794e7c
Brian Serein <bserein22@gmail.com>
2022-02-16 13:56:58 -0500

    added log out button and login page for sign in

--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -1,10 +1,64 @@
-import { Link } from "react-router-dom";
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { app } from '../ConnectAuth'
 
-export default function Login() {
-  return (
-    <>
-      <h1>Login</h1>
-    <p>Not a user? <Link to="/signup">Sign up</Link></p>
-    </>
-  );
+export default function Login({ setUser }){
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const navigate = useNavigate();
+const provider = new GoogleAuthProvider();
+// this is a class so you substantiate it with new
+//this will be for the google sign in
+const auth = getAuth(app);
+const handleFormSubmit = (e) => {
+    e.preventDefault()
+    signInWithEmailAndPassword(auth, email, password)
+    //change this component to sign in so it remembers the user
+    .then(result => {
+        //setUser
+        setUser(result.user)
+        //navigate to home
+        navigate('/')
+        //this will take you to the welcome page
+    })
+    .catch(alert)
+    // alert(`Trying to sign up as ${email}`); no longer need this since its connected to firebase
+}
+//this function is used for the sign up using whatever email you wish
+const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+    //this will return a promise
+    .then(result => {
+        //setUser
+        setUser(result.user)
+        //navigate to home
+        navigate('/')
+        //this will take you to the welcome page
+    })
+    .catch(alert)
+}
+    return (
+        <>
+        <h1>Login</h1>
+        <hr />
+        <form onSubmit={handleFormSubmit}>
+                {/* form friendly works for the password and the email because its on the form and everything from under this one will be accessible */}
+            <label>Email: <input type="email" value={email} onChange={e => setEmail(e.target.value)}/></label> <br />
+            {/* with the type email it already lets it know that you need to put the @ for the email */}
+            <label>Password: <input type="password" value={password} onChange={e => setPassword(e.target.value)}/></label> <br />
+            {/* this took control of the inputs and have a prevent default so the page doesnt reload when its inputted */}
+            <input type="submit" value="Login"/>
+        </form>
+        <button 
+        onClick={handleGoogleLogin}
+        // clickable function 
+        style={{
+            backgroundColor: 'black', 
+            color: 'white', 
+            border: 'none'}}>
+                Sign in with Google</button>
+        <p>Not a user? <Link to="/signup">Sign up</Link></p>
+        </>
+    )
 }
--- src/scenes/Welcome.js
+++ src/scenes/Welcome.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 //you also can delete the import react
 
 //you can also put the export default behind the function
-export default function Welcome(){
+export default function Welcome({user, setUser}){
+    console.log(user) //email, displayName, photoURL
     return (
         <>
         <h1>Welcome</h1>
-        <p>Want to leave? <Link to="/signup">logout</Link></p>
+        <h2>{user.displayName || user.email}</h2>
+        {user.photoURL && <img src={user.photoURL} alt={'Profile phot0 of logged-in user'} />} <br/>
+        {/* had to put a zero because it doesnt like it describing it as photo or picture */}
+        <button onClick={() => setUser(false)}>Sign out</button>
         </>
     )
 }
